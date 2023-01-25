e9643f3e3b48557c62f7ca92b5119110ef3552e8
AironRuda <airon.ruda@udea.edu.co>
2023-01-25 18:49:50 -0500

    feat: profilephoto and displayName on screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <span className="logo">Chat App</span>
       <div className="user">
-        <img
-          src="https://www.shutterstock.com/image-photo/laughing-caucasian-female-student-group-260nw-1870386598.jpg"
-          alt=""
-        />
-        <span>Airon</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>Log Out</button>
       </div>
     </div>
